d61ab7c5e8b202840d706ecdccdee614f720a7ca
KevinArmache <kevinarmache@gmail.com>
2021-11-16 14:33:33 +0100

    feature: style contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,25 @@
-import React from "react"
-import SectionTitle from "../shared/section_title/SectionTitle"
-import "./contact.scss"
+import React from "react";
+import SectionTitle from "../shared/section_title/SectionTitle";
+import "./contact.scss";
 
 const Contact = () => {
   return (
     <section className="contact-section" id="contact">
-      <SectionTitle title= "CONTACT"  />
+      <SectionTitle title="CONTACT" />
       <form action="#" method="post">
-        <input type="text" name="userName" id="" />
-        <input type="email" name="" id="" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <input type="text" name="userName" id="" placeholder="Name" />
+        <input type="email" name="" id="" placeholder="Email" />
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Your text here"
+        />
         <button type="submit">Envoyer</button>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
